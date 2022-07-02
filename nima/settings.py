7b018ed13f784215487e3216c5fbac726cc4eea6
Ashkan Sarshary <sarshary@gmail.com>
2022-07-02 18:20:12 +0430

    'debuhhh'

--- nima/settings.py
+++ nima/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d0nxfg5y*l6o15-%0r5w@^8-_8r2v0l*hp51&19qddo3k7#pfz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['nimajewelry.herokuapp.com']
 
